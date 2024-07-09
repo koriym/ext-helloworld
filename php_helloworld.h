c74935822dd284897e25869215515cc44405ed27
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-07-09 23:12:50 +0900

    Refactor PHP helloworld header file
    The PHP helloworld header file has been refactored for improved clarity and code understanding. The definitions and variable access macros are updated to explicitly state their functionalities. The helloworld extension version is also modified. Comments have been added for each block of code to strengthen the code readability.

--- php_helloworld.h
+++ php_helloworld.h
@@ -1,37 +1,34 @@
-#ifndef PHP_HELLOWORLD_H
-#define PHP_HELLOWORLD_H
+#ifndef PHP_HELLOWORLD_H // If PHP_HELLOWORLD_H is not defined
+#define PHP_HELLOWORLD_H // Define PHP_HELLOWORLD_H (header guard)
 
-extern zend_module_entry helloworld_module_entry;
-#define phpext_helloworld_ptr &helloworld_module_entry
+extern zend_module_entry helloworld_module_entry;  // Declare helloworld module entry as external reference
+#define phpext_helloworld_ptr &helloworld_module_entry  // Define pointer to helloworld module
 
-#define PHP_HELLOWORLD_VERSION "1.0"
+#define PHP_HELLOWORLD_VERSION "0.1.0" // Define helloworld extension version
 
-typedef struct _zend_helloworld_globals {
-    char *greeting;
-} zend_helloworld_globals;
-
-#ifdef PHP_WIN32
-#   define PHP_HELLOWORLD_API __declspec(dllexport)
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#   define PHP_HELLOWORLD_API __attribute__ ((visibility("default")))
-#else
-#   define PHP_HELLOWORLD_API
+#ifdef PHP_WIN32  // If in Windows environment
+#define PHP_HELLOWORLD_API __declspec(dllexport)  // Specify DLL export
+#elif defined(__GNUC__) && __GNUC__ >= 4  // If using GCC 4 or later
+#define PHP_HELLOWORLD_API __attribute__ ((visibility("default")))  // Specify default visibility
+#else  // For other environments
+#define PHP_HELLOWORLD_API  // No specific definition
 #endif
 
 #ifdef ZTS
 #include "TSRM.h"
 #endif
 
-PHP_FUNCTION(helloworld);
+PHP_FUNCTION(helloworld); // helloworld() function
 
-PHP_MINIT_FUNCTION(helloworld);
-PHP_MSHUTDOWN_FUNCTION(helloworld);
-PHP_MINFO_FUNCTION(helloworld);
+// Function declarations
+PHP_MINIT_FUNCTION(helloworld); // Module initialization function
+PHP_MSHUTDOWN_FUNCTION(helloworld); // Module shutdown function
+PHP_MINFO_FUNCTION(helloworld);  // Module information function
 
-#ifdef ZTS
-#define HELLOWORLD_G(v) TSRMG(helloworld_globals_id, zend_helloworld_globals *, v)
-#else
-#define HELLOWORLD_G(v) (helloworld_globals.v)
+#ifdef ZTS  // If in thread-safe mode
+#define HELLOWORLD_G(v) TSRMG(helloworld_globals_id, zend_helloworld_globals *, v)  // Global variable access macro (thread-safe version)
+#else // If in non-thread-safe mode
+#define HELLOWORLD_G(v) (helloworld_globals.v)  // Global variable access macro (non-thread-safe version)
 #endif
 
 #endif  /* PHP_HELLOWORLD_H */
